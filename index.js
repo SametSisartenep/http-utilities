69c035fdabe8b13ce6185c398120eba1d71d61ec
SametSisartenep <krashbuffer@gmail.com>
2014-11-02 17:05:47 +0100

    status codes interface created. Dr.Mime added.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var codes = require('./stat-codes.json'),
+  drMime = require('dr-mime');
 
+var httpMethods = ['GET', 'POST', 'PUT', 'DELETE'];
+
+function statCode ( name ) {
+  return codes[name];
+}
+
+function statName ( code ) {
+  for (var key in codes)
+  {
+    if (codes[key] === code)
+    {
+      return key;
+    }
+  }
+  return false;
+}
