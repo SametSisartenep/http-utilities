87b7b4a9233b31a58053c15b794f8c7126f6dfe7
SametSisartenep <krashbuffer@gmail.com>
2014-12-03 14:18:11 +0100

    Init function added.

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-var codes = require('./stat-codes.json'),
+var hardCodes = require('./stat-codes.json'),
+  tinyCodes = require('./tiny-status-codes.json'),
   drMime = require('dr-mime');
 
-function statCode ( name ) {
-  return codes[name];
-}
+var httpUtils = function () {};
 
-function statName ( code ) {
-  for (var key in codes)
+module.exports = exports = function ( type ) {
+  var codes = null;
+
+  if (type)
   {
-    if (codes[key] === code)
+    if (type === 'tiny')
     {
-      return key;
+      codes = tinyCodes;
+    }
+    else if (type === 'hard')
+    {
+      codes = hardCodes;
+    }
+    else
+    {
+      throw new Error('');
     }
   }
-  return false;
-}
+  else
+  {
+    codes = hardCodes;
+  }
+
+  httpUtils.prototype.statCode = function statCode ( name ) {
+    return codes[name];
+  };
+    
+  httpUtils.prototype.statName = function statName ( code ) {
+    for (var key in codes)
+    {
+      if (codes[key] === code)
+      {
+        return key;
+      }
+    }
+    return false;
+  };
+
+  return new httpUtils();
+};
